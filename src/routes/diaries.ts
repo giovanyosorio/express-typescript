9a1e906063bd9d885d74d48491cb65d9b61d6e30
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-05 17:33:43 +0000

    add entry

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -8,8 +8,18 @@ res.send(diaryService.getEntriesWithOutSensitiveInfo())
 })
 
 
+router.get("/:id",(_req,res)=>{
+    const diary = diaryService.findById(Number(_req.params.id))
+    return diary
+    ?res.send(diary)
+    :res.sendStatus(404)
+
+})
+
 router.post("/",(_req,res)=>{
-    res.send("saving a diary")
+    const {date,weather,visibility,comment}=_req.body
+    const newDiaryEntry = diaryService.addEntry(date,weather,visibility,comment)
+    
 })
 
 export default router
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,12 +1,26 @@
 import diaryData from './diaries.json';
-import { DiaryEntry,NonSensitiveDiaryEntry } from '../types';
+import { DiaryEntry,NonSensitiveDiaryEntry, Weather, visibility } from '../types';
 
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
 
 export const getEntries = () :DiaryEntry[]=> diaries
 
+export const findById = (id:number):DiaryEntry|undefined=>{
+    const entry = diaries.find(d=>d.id===id)
+    return entry
+}
 export const getEntriesWithOutSensitiveInfo = () :NonSensitiveDiaryEntry[]=>{
     return diaries.map(({id,date,weather,visibility})=>({id,date,weather,visibility}))
 }
 
-export const addEntry = () :undefined=> undefined
+export const addEntry =(date:string,weather:Weather,visibility:visibility,comment:string):DiaryEntry=>{
+    const newDiaryEntry = {
+        id:Math.max(...diaries.map(d=>d.id))+1,
+        date,
+        weather,
+        visibility,
+        comment
+    }
+    diaries.push(newDiaryEntry)
+    return newDiaryEntry
+}
